a6920fe354655ed815a9e5cdcf30010525793716
Alessandro Dias <blackmordor@hotmail.com>
2017-10-31 20:36:08 -0200

    Update IngressoInfantilTest.kt

--- src/test/com/alessandrovdias/umovmetickets/model/IngressoInfantilTest.kt
+++ src/test/com/alessandrovdias/umovmetickets/model/IngressoInfantilTest.kt
@@ -1,63 +1,51 @@
-package com.alessandrovdias.umovmetickets.model;
-
-import static org.junit.Assert.*;
-
-import org.junit.Test;
-
-/** 
- * Testes unitários para a classe IngressoInfantil
- * 
- * @author Alessandro
- *
- */
-public class IngressoInfantilTest {
-
-	@Test
-	public void testGetValorIngressoDomingo() {
-		IngressoInfantil ingresso = new IngressoInfantil();
-        assertEquals(new Double(5.5), ingresso.getValorIngressoDomingo());	
-    }
-
-	@Test
-	public void testGetValorIngressoSegundaFeira() {
-		IngressoInfantil ingresso = new IngressoInfantil();
-        assertEquals(new Double(4.95), ingresso.getValorIngressoSegundaFeira());	
-	}
-
-	@Test
-	public void testGetValorIngressoTercaFeira() {
-		IngressoInfantil ingresso = new IngressoInfantil();
-        assertEquals(new Double(4.68), ingresso.getValorIngressoTercaFeira());	
-	}
-
-	@Test
-	public void testGetValorIngressoQuartaFeira() {
-		IngressoInfantil ingresso = new IngressoInfantil();
-        assertEquals(new Double(3.85), ingresso.getValorIngressoQuartaFeira());	
-	}
-
-	@Test
-	public void testGetValorIngressoQuintaFeira() {
-		IngressoInfantil ingresso = new IngressoInfantil();
-        assertEquals(new Double(5.5), ingresso.getValorIngressoQuintaFeira());	
-	}
-
-	@Test
-	public void testGetValorIngressoSextaFeira() {
-		IngressoInfantil ingresso = new IngressoInfantil();
-        assertEquals(new Double(4.9), ingresso.getValorIngressoSextaFeira());	
-	}
-
-	@Test
-	public void testGetValorIngressoSabado() {
-		IngressoInfantil ingresso = new IngressoInfantil();
-        assertEquals(new Double(5.5), ingresso.getValorIngressoSabado());	
-	}
-
-	@Test
-	public void testGetValorIngressoFeriado() {
-		IngressoInfantil ingresso = new IngressoInfantil();
-        assertEquals(new Double(5.5), ingresso.getValorIngressoFeriado());	
-	}
-
+package com.alessandrovdias.umovmetickets.model
+import org.junit.Assert.*
+import org.junit.Test
+/**
+* Testes unitários para a classe IngressoInfantil
+*
+* @author Alessandro
+*
+*/
+class IngressoInfantilTest {
+  @Test
+  fun testGetValorIngressoDomingo() {
+    val ingresso = IngressoInfantil()
+    assertEquals(5.5, ingresso.getValorIngressoDomingo())
+  }
+  @Test
+  fun testGetValorIngressoSegundaFeira() {
+    val ingresso = IngressoInfantil()
+    assertEquals(4.95, ingresso.getValorIngressoSegundaFeira())
+  }
+  @Test
+  fun testGetValorIngressoTercaFeira() {
+    val ingresso = IngressoInfantil()
+    assertEquals(4.68, ingresso.getValorIngressoTercaFeira())
+  }
+  @Test
+  fun testGetValorIngressoQuartaFeira() {
+    val ingresso = IngressoInfantil()
+    assertEquals(3.85, ingresso.getValorIngressoQuartaFeira())
+  }
+  @Test
+  fun testGetValorIngressoQuintaFeira() {
+    val ingresso = IngressoInfantil()
+    assertEquals(5.5, ingresso.getValorIngressoQuintaFeira())
+  }
+  @Test
+  fun testGetValorIngressoSextaFeira() {
+    val ingresso = IngressoInfantil()
+    assertEquals(4.9, ingresso.getValorIngressoSextaFeira())
+  }
+  @Test
+  fun testGetValorIngressoSabado() {
+    val ingresso = IngressoInfantil()
+    assertEquals(5.5, ingresso.getValorIngressoSabado())
+  }
+  @Test
+  fun testGetValorIngressoFeriado() {
+    val ingresso = IngressoInfantil()
+    assertEquals(5.5, ingresso.getValorIngressoFeriado())
+  }
 }
